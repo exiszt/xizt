8ff77b44d366894ea2a0d2ab0c6a86752d2e6b10
Exequiel-Carrizo <exzer11@gmail.com>
2022-11-09 01:35:56 -0300

    Servidor con Express

--- server.js
+++ server.js
@@ -1,28 +1,95 @@
+const fs = require('fs')
 const express = require('express')
-
 const app = express()
-const PORT = 8080
+const PORT = process.env.PORT || 8090
 
-let visitas = 0
+class Contenedor {
+    constructor(name) {
+        this.name = name
+    }
 
-app.get('/', (req, res)=>{
-    res.send(`<h1 style='color:blue'>Hola mundo</h1>`)
+    async save(item) {
+        try {
+            let saveData = []
+            if (fs.existsSync('./productos.txt')) {
+                const currentData = await fs.promises.readFile('./productos.txt', 'utf-8')
+                if (currentData !== '') {
+                    saveData = JSON.parse(currentData)
+                }
+            }
+
+            item.id = saveData.length + 1
+            saveData.push(item)
+            await fs.promises.writeFile('./productos.txt', JSON.stringify(saveData, null, 2))
+            return item.title
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+
+    async getById(id) {
+        try {
+            const currentData = await fs.promises.readFile('./productos.txt', 'utf-8')
+            const dataJSON = JSON.parse(currentData)
+            return dataJSON.find(element => element.id === id) ?? null
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+
+    async getAll() {
+        try {
+            const currentData = await fs.promises.readFile('./productos.txt', 'utf-8')
+            return JSON.parse(currentData)
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+
+    async deleteById(id) {
+        try {
+            const currentData = await fs.promises.readFile('./productos.txt', 'utf-8')
+            const dataJSON = JSON.parse(currentData)
+            const newDataJSON = dataJSON.filter(element => element.id !== id)
+            const newIdDataJSON = newDataJSON.map(element => element.id = newDataJSON.indexOf(element) + 1)
+            console.log(newIdDataJSON)
+            await fs.promises.writeFile('./productos.txt', JSON.stringify(newDataJSON, null, 2))
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+
+    async deleteAll() {
+        try {
+            fs.promises.writeFile('./productos.txt', '')
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+const productos = new Contenedor('./productos.txt')
+
+app.get('/', (req, res) => {
+    res.send(`/productos /random`)
 })
 
-// Visitas
-app.get('/visitas', (req, res)=>{
-    visitas += 1
-    res.send(`La cantidad de visitas es: ${visitas}`)
+app.get('/productos', async (req, res) => {
+    const showProds = await productos.getAll()
+    res.send(showProds)
 })
 
-// Fecha y hora
-app.get('/fyh', (req, res) =>{
-    const date = new Date
-    res.send(date.toLocaleString())
+app.get('/random', async (req, res) => {
+    const prod = await productos.getAll()
+    const randomNum = Math.floor(Math.random() * prod.length)
+    res.send(prod[randomNum])
 })
 
-const server = app.listen(PORT, ()=>{
-    console.log(`Servidor express escuchando en el puerto: ${PORT}`)
+app.listen(PORT, () => {
+    console.log(`Escuchando en el puerto: ${PORT}`)
 })
-
-server.on('error', error => console.log(`Error en el servidor: ${error}`))
